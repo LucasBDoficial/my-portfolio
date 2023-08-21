6aaced696b265756734d980f9b7dcd0069aeee62
LucasBD <jlucasbucalon@gmail.com>
2023-08-21 04:05:17 -0300

    home 02

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,11 +5,28 @@ import Link from "next/link";
 
 function toggleMode() {
     const html = document.documentElement
+    const sun = document.querySelector('#sun')
+    const img1 = document.querySelector('#image1')
+    const img2 = document.querySelector('#image2')
+
         if( html.classList.contains('root')){
             html.classList.remove('root')
             html.classList.add('ligth')
+            
+            sun.classList.remove('moon')
+            sun.classList.add('moon2')
+
+            img1.classList.add('bgi')
+            img2.classList.remove('bgi')
+            
         }else{  html.classList.remove('ligth')
                 html.classList.add('root')
+                
+                sun.classList.add('moon')
+
+                img1.classList.remove('bgi')
+                img2.classList.add('bgi')
+
             }
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,37 +5,49 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <motion.main 
-    initial={{ opacity: 0 }}
-    animate={{ opacity: 1, y:[0]}}
-    transition={{
-    duration: 0.5,
-    delay: 0.5,
-    }} > 
+    <motion.main className="flex pt-[30px] 
+                max-[900px]:flex-wrap max-[900px]:h-0 max-[900px]:justify-items-start"
+
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, y:[0]}}
+      transition={{
+      duration: 0.5,
+      delay: 0.5,
+      }
+    }> 
 
       <Image src="/images/home2.png" alt="1223" width={200} height={200} 
         className="absolute top-0 right-0"/>
 
-      <Image src="/images/home1.png" alt="1223" width={550} height={550} 
-          className="absolute top-40 left-20 max-lg:left-[25%] "/>
+      <div className="w-[50%] relative flex flex-col justify-center max-[900px]:w-full">     
 
-      <div className="flex w-hll max-lg:justify-center">        
-        <Image src="/images/perfil.png" alt="1223" width={470} height={470} 
-          className="absolute top-28"/>
+          <Image src="/images/home1.png" alt="1223" width={550} height={550} 
+            className="absolute z-0 ml-[25%] mt-[30px]"/>
+    
+          <Image src="/images/perfil.png" alt="1223" width={470} height={470} 
+            className="relative m-auto" id="image1"/>
+
+          <Image src="/images/perfil2.png" alt="1223" width={470} height={470} 
+            className="relative m-auto bgi" id="image2"/>
+            
+          <div className="sun old m-auto mt-[-20px]">
+            <samp id="sun" className="moon"></samp>
+          </div>
+
       </div>
 
-      <div className="absolute ml-[50%] flex flex-col h-full justify-center 
-      max-[900px]:ml-auto max-[900px]:w-full max-[900px]:text-center max-[900px]:top-[25px] max-[844px]:top-[515px] 
-      max-[768px]:top-[175px]">
+      <div className="w-[50%] flex justify-center items-center mr-[5%] mt-[40px]
+          max-[900px]:w-full max-[900px]:items-start max-[900px]:mr-[0] ">
 
-        <h2 className="font-black text-[3.5rem] leading-[40px]">Tansformando ideias <br/> em <span>REALIDADE</span> </h2>
+        <div className="absolute text-center">
+          <h2 className="font-black text-[3.5rem] leading-[40px]">Tansformando ideias <br/> em <span>REALIDADE</span> </h2>
 
-        <p className="mt-[15px]">“ Não é a linguagem de programação que define <br/> o programador, mas sim sua lógica. ”</p>
+          <p className="mt-[15px]">“ Não é a linguagem de programação que define <br /> o programador, mas sim sua lógica. ”</p>
 
-        <p className="italic text-[1.2rem] text-right mt-[10px] max-[900px]:text-center">David Ribeiro Guilherme</p>
+          <p className="italic text-[1.2rem] text-right mt-[10px]">David Ribeiro Guilherme</p>
 
-        <p className=" text-[1.4rem] italic opacity-[60%] text-center mt-[40px]">Designer e desenvolvedor: Lucas Bucalon</p>
-
+          <p className=" text-[1.4rem] italic opacity-[60%] text-center mt-[40px]">Designer e desenvolvedor: Lucas Bucalon</p>
+        </div>
       </div>
 
     </motion.main>
