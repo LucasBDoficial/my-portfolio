d99b35e37f352a0972d7b2a69e8a9da9572d3a11
LucasBD <jlucasbucalon@gmail.com>
2023-08-24 17:00:47 -0300

    work 03

--- src/app/Work/page.tsx
+++ src/app/Work/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 import { motion } from "framer-motion";
-import  ButtonMode  from "../components/ButtonMode"
+import { useState, useEffect, useRef } from "react";
+import  ButtonMode  from "../components/ButtonMode";
 import Image from "next/image";
 
 export default function Work(){
+
     return(
         <motion.section className="flex flex-wrap justify-center "
         initial={{ opacity: 0 }}
@@ -23,9 +25,42 @@ export default function Work(){
                 className=" absolute bottom-0 right-0 z-0 animate-pulse "/>
 
             <div className="flex flex-wrap justify-center relative w-full">
-                <h2 className="px-[2%] font-black text-[3.5rem] mt-[90px]" >Meu trabalho é solucionar <span> PROBLEMAS </span></h2>
+                <h2 className="px-[2%] font-black text-[3.5rem] mt-[100px]" >Meu trabalho é solucionar <span> PROBLEMAS </span></h2>
                 
-                <div className="w-full h-full bg-slate-50 "></div>
+
+
+
+
+
+                <div className="mx-auto flex justify-center mt-[50px]">
+
+                    <motion.div className="max-w-[100%] cursor-grab" whileTap={{ cursor: "grabbing"}}>
+
+                        <motion.div className="flex"
+                        drag="x"
+                        >                               
+                            <div id="box" className="flex w-full snap-start">
+                                <div id="box" className="min-w-[200px] min-h-[200px] bg-amber-500 mx-[20px]"></div>
+                                <div id="box" className="min-w-[200px] min-h-[200px] bg-amber-500 mx-[20px]"></div>
+                                <div id="box" className="min-w-[200px] min-h-[200px] bg-amber-500 mx-[20px]"></div>
+                            </div>
+
+                            {/* <div className="flex w-full">
+                                <div id="box" className="min-w-[200px] min-h-[200px] bg-amber-500 mx-[20px]"></div>
+                                <div id="box" className="min-w-[200px] min-h-[200px] bg-amber-500 mx-[20px]"></div>
+                                <div id="box" className="min-w-[200px] min-h-[200px] bg-amber-500 mx-[20px]"></div>
+                            </div>
+                              
+                            <div className="flex w-full">
+                                <div id="box" className="min-w-[200px] min-h-[200px] bg-amber-500 mx-[20px]"></div>
+                                <div id="box" className="min-w-[200px] min-h-[200px] bg-amber-500 mx-[20px]"></div>
+                                <div id="box" className="min-w-[200px] min-h-[200px] bg-amber-500 mx-[20px]"></div>
+                            </div> */}
+
+                        </motion.div>
+                    </motion.div>
+                </div>
+
             </div>
 
             <div onClick={toggleMode}><ButtonMode /></div>
@@ -39,4 +74,3 @@ function toggleMode(){
     
         if(html.classList.contains("light")){}     
 }
-
