36c46eced2addfc4e8d960c5e5e349794b41a84f
LucasBD <jlucasbucalon@gmail.com>
2023-09-02 01:25:55 -0300

    final 01

--- src/app/components/File.tsx
+++ src/app/components/File.tsx
@@ -36,7 +36,7 @@ export default function File(){
                                         
                     <div className='hover:bg-gray-900/60 w-full h-full absolute flex justify-center items-center'>
 
-                        <p className='group-hover:flex absolute hidden items-center text-[2rem] font-medium' >VISITAR PROJETO
+                        <p className='group-hover:flex text-white absolute hidden items-center text-[2rem] font-medium' >VISITAR PROJETO
                     
                         <svg xmlns="http://www.w3.org/2000/svg" width="2.4rem" height="2.4rem" viewBox="0 0 1024 1024"><path fill="currentColor" d="M754.752 480H160a32 32 0 1 0 0 64h594.752L521.344 777.344a32 32 0 0 0 45.312 45.312l288-288a32 32 0 0 0 0-45.312l-288-288a32 32 0 1 0-45.312 45.312L754.752 480z"/></svg>
 
@@ -52,7 +52,7 @@ export default function File(){
                                         
                     <div className='hover:bg-gray-900/60 w-full h-full absolute flex justify-center items-center'>
 
-                        <p className='group-hover:flex absolute hidden items-center text-[2rem] font-medium' >VISITAR PROJETO
+                        <p className='group-hover:flex text-white absolute hidden items-center text-[2rem] font-medium' >VISITAR PROJETO
                     
                         <svg xmlns="http://www.w3.org/2000/svg" width="2.4rem" height="2.4rem" viewBox="0 0 1024 1024"><path fill="currentColor" d="M754.752 480H160a32 32 0 1 0 0 64h594.752L521.344 777.344a32 32 0 0 0 45.312 45.312l288-288a32 32 0 0 0 0-45.312l-288-288a32 32 0 1 0-45.312 45.312L754.752 480z"/></svg>
 
