4a5e21f0449befbd97fe267fab854109856618d4
LucasBD <jlucasbucalon@gmail.com>
2023-09-05 16:16:23 -0300

    home style 02

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
 
       </div> 
 
-      <div className="w-[50%] flex justify-center items-center mr-[5%] mt-[40px] z-20
+      <div className="w-[50%] flex justify-center items-center mr-[5%] mt-[40px] z-20 pb-[20px]
           max-[900px]:w-full max-[900px]:items-start max-[900px]:mr-[0] ">
 
         <div className="absolute text-center">
@@ -49,7 +49,7 @@ export default function Home() {
 
           <p className="italic text-[1.2rem] text-right mt-[10px]">David Ribeiro Guilherme</p>
 
-          <p className=" text-[1.4rem] italic opacity-[60%] text-center my-[40px]">Tema claro ou escuro de acordo com o padão do navegador</p>
+          <p className=" text-[1.4rem] italic opacity-[60%] text-center mt-[40px]">Tema claro ou escuro de acordo com o padão do navegador</p>
         </div>
       </div>
 
